b3c9d7f8edac3f8400bde4b6a52aca86a10b4da0
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-03-30 19:06:50 +0100

    grid update

--- Laboratorio Creativo/js/script.js
+++ Laboratorio Creativo/js/script.js
@@ -1,7 +1,21 @@
-let clicked = true;
+let clicked = true
 
-function expand() {
+document.querySelector("button").addEventListener("click", function() {
+    document.querySelector(".contentWrapper").classList.toggle("expand");
+    if (clicked) {
+        document.getElementById("arrow").style.transform = "rotate(180deg)";
+        document.getElementById("espandi").innerHTML = "Restringi";
+        clicked = false;
+    } else {
+        document.getElementById("arrow").style.transform = "rotate(0deg)";
+        document.getElementById("espandi").innerHTML = "Espandi";
+        clicked = true;
+    }
+});
 
+
+/*function expand() {
+        document.getElementById("wrapper").style.transition = "height 2s ease 1s";
     if (clicked) {
         document.getElementById("wrapper").style.height = "fit-content";
         document.getElementById("intro").style.overflow = "visible";
@@ -9,10 +23,10 @@ function expand() {
         document.getElementById("espandi").innerHTML = "Restringi";
         clicked = false;
     } else {
-        document.getElementById("wrapper").style.height = "100px";
+        document.getElementById("wrapper").style.height = "50px";
         document.getElementById("intro").style.overflow = "hidden";
         document.getElementById("arrow").style.transform = "rotate(0deg)";
         document.getElementById("espandi").innerHTML = "Espandi";
         clicked = true;
     }
-};
+};*/
