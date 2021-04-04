c765d94853501096f19133c3a909cb140d38274c
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-04-04 17:35:12 +0100

    headline with contact added

--- Laboratorio Creativo/js/script.js
+++ Laboratorio Creativo/js/script.js
@@ -8,25 +8,7 @@ document.querySelector("button").addEventListener("click", function() {
         clicked = false;
     } else {
         document.getElementById("arrow").style.transform = "rotate(0deg)";
-        document.getElementById("espandi").innerHTML = "Espandi";
+        document.getElementById("espandi").innerHTML = "Per saperne di piú clicca quí";
         clicked = true;
     }
 });
-
-
-/*function expand() {
-        document.getElementById("wrapper").style.transition = "height 2s ease 1s";
-    if (clicked) {
-        document.getElementById("wrapper").style.height = "fit-content";
-        document.getElementById("intro").style.overflow = "visible";
-        document.getElementById("arrow").style.transform = "rotate(180deg)";
-        document.getElementById("espandi").innerHTML = "Restringi";
-        clicked = false;
-    } else {
-        document.getElementById("wrapper").style.height = "50px";
-        document.getElementById("intro").style.overflow = "hidden";
-        document.getElementById("arrow").style.transform = "rotate(0deg)";
-        document.getElementById("espandi").innerHTML = "Espandi";
-        clicked = true;
-    }
-};*/
