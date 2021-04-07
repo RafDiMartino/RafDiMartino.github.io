14d213b8ea0820538d087b3eb70da49d656413df
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-04-07 14:24:53 +0100

    home page slider

--- Laboratorio Creativo/js/carousel.js
+++ Laboratorio Creativo/js/carousel.js
@@ -1,8 +1,8 @@
 document.querySelector(".dot0").addEventListener("click", function() {
-    document.querySelector(".banner0").style.opacity = "1";
-    document.querySelector(".banner1").style.opacity = "0";
-    document.querySelector(".banner2").style.opacity = "0";
-    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".about0").style.opacity = "1";
+    document.querySelector(".about1").style.opacity = "0";
+    document.querySelector(".about2").style.opacity = "0";
+    document.querySelector(".about3").style.opacity = "0";
     document.querySelector(".dot0").style.color = "rgb(255, 213, 25)"
     document.querySelector(".dot1").style.color = "white"
     document.querySelector(".dot2").style.color = "white"
@@ -10,10 +10,10 @@ document.querySelector(".dot0").addEventListener("click", function() {
 })
 
 document.querySelector(".dot1").addEventListener("click", function() {
-    document.querySelector(".banner0").style.opacity = "0";
-    document.querySelector(".banner1").style.opacity = "1";
-    document.querySelector(".banner2").style.opacity = "0";
-    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "1";
+    document.querySelector(".about2").style.opacity = "0";
+    document.querySelector(".about3").style.opacity = "0";
     document.querySelector(".dot0").style.color = "white"
     document.querySelector(".dot1").style.color = "rgb(255, 213, 25)"
     document.querySelector(".dot2").style.color = "white"
@@ -21,10 +21,10 @@ document.querySelector(".dot1").addEventListener("click", function() {
 })
 
 document.querySelector(".dot2").addEventListener("click", function() {
-    document.querySelector(".banner0").style.opacity = "0";
-    document.querySelector(".banner1").style.opacity = "0";
-    document.querySelector(".banner2").style.opacity = "1";
-    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "0";
+    document.querySelector(".about2").style.opacity = "1";
+    document.querySelector(".about3").style.opacity = "0";
     document.querySelector(".dot0").style.color = "white"
     document.querySelector(".dot1").style.color = "white"
     document.querySelector(".dot2").style.color = "rgb(255, 213, 25)"
@@ -32,10 +32,10 @@ document.querySelector(".dot2").addEventListener("click", function() {
 })
 
 document.querySelector(".dot3").addEventListener("click", function() {
-    document.querySelector(".banner0").style.opacity = "0";
-    document.querySelector(".banner1").style.opacity = "0";
-    document.querySelector(".banner2").style.opacity = "0";
-    document.querySelector(".banner3").style.opacity = "1";
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "0";
+    document.querySelector(".about2").style.opacity = "0";
+    document.querySelector(".about3").style.opacity = "1";
     document.querySelector(".dot0").style.color = "white"
     document.querySelector(".dot1").style.color = "white"
     document.querySelector(".dot2").style.color = "white"
