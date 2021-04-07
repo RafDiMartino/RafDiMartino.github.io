9d582b53903d0bdce90bf9406cd15182c71641cd
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-04-07 14:36:21 +0100

    swipe test

--- Laboratorio Creativo/js/carousel.js
+++ Laboratorio Creativo/js/carousel.js
@@ -43,13 +43,48 @@ document.querySelector(".dot3").addEventListener("click", function() {
 })
 
 
-document.addEventListener('swiped-left', function() {
-    document.querySelector(".about0").style.opacity = "1";
-    document.querySelector(".about1").style.opacity = "0";
+//swipe
+
+document.querySelector(".dot1").addEventListener("swipe-left", function() {
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "1";
     document.querySelector(".about2").style.opacity = "0";
     document.querySelector(".about3").style.opacity = "0";
-    document.querySelector(".dot0").style.color = "rgb(255, 213, 25)"
-    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "rgb(255, 213, 25)"
     document.querySelector(".dot2").style.color = "white"
     document.querySelector(".dot3").style.color = "white"
-});
+})
+
+document.querySelector(".dot1").addEventListener("swipe-left", function() {
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "1";
+    document.querySelector(".about2").style.opacity = "0";
+    document.querySelector(".about3").style.opacity = "0";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "rgb(255, 213, 25)"
+    document.querySelector(".dot2").style.color = "white"
+    document.querySelector(".dot3").style.color = "white"
+})
+
+document.querySelector(".dot2").addEventListener("swipe-left", function() {
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "0";
+    document.querySelector(".about2").style.opacity = "1";
+    document.querySelector(".about3").style.opacity = "0";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot2").style.color = "rgb(255, 213, 25)"
+    document.querySelector(".dot3").style.color = "white"
+})
+
+document.querySelector(".dot3").addEventListener("swipe-left", function() {
+    document.querySelector(".about0").style.opacity = "0";
+    document.querySelector(".about1").style.opacity = "0";
+    document.querySelector(".about2").style.opacity = "0";
+    document.querySelector(".about3").style.opacity = "1";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot2").style.color = "white"
+    document.querySelector(".dot3").style.color = "rgb(255, 213, 25)"
+})
