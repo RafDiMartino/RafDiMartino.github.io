4bc37c9d7eba357512007fbf8c95ff2a548879cb
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-04-07 13:39:52 +0100

    home page slider

---
+++ Laboratorio Creativo/js/carousel.js
@@ -0,0 +1,43 @@
+document.querySelector(".dot0").addEventListener("click", function() {
+    document.querySelector(".banner0").style.opacity = "1";
+    document.querySelector(".banner1").style.opacity = "0";
+    document.querySelector(".banner2").style.opacity = "0";
+    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".dot0").style.color = "rgb(255, 213, 25)"
+    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot2").style.color = "white"
+    document.querySelector(".dot3").style.color = "white"
+})
+
+document.querySelector(".dot1").addEventListener("click", function() {
+    document.querySelector(".banner0").style.opacity = "0";
+    document.querySelector(".banner1").style.opacity = "1";
+    document.querySelector(".banner2").style.opacity = "0";
+    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "rgb(255, 213, 25)"
+    document.querySelector(".dot2").style.color = "white"
+    document.querySelector(".dot3").style.color = "white"
+})
+
+document.querySelector(".dot2").addEventListener("click", function() {
+    document.querySelector(".banner0").style.opacity = "0";
+    document.querySelector(".banner1").style.opacity = "0";
+    document.querySelector(".banner2").style.opacity = "1";
+    document.querySelector(".banner3").style.opacity = "0";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot2").style.color = "rgb(255, 213, 25)"
+    document.querySelector(".dot3").style.color = "white"
+})
+
+document.querySelector(".dot3").addEventListener("click", function() {
+    document.querySelector(".banner0").style.opacity = "0";
+    document.querySelector(".banner1").style.opacity = "0";
+    document.querySelector(".banner2").style.opacity = "0";
+    document.querySelector(".banner3").style.opacity = "1";
+    document.querySelector(".dot0").style.color = "white"
+    document.querySelector(".dot1").style.color = "white"
+    document.querySelector(".dot2").style.color = "white"
+    document.querySelector(".dot3").style.color = "rgb(255, 213, 25)"
+})
