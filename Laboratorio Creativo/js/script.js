2f7fbba70f25eb0cc81e85335604ec46208b4bfb
R4FFF <69420740+R4FFF@users.noreply.github.com>
2021-08-10 11:05:28 +0100

    js refactor

--- Laboratorio Creativo/js/script.js
+++ Laboratorio Creativo/js/script.js
@@ -1,6 +1,9 @@
-let clicked = true
+
 
 document.querySelector("button").addEventListener("click", () => {
+
+    let clicked = true
+
     document.querySelector(".contentWrapper").classList.toggle("expand");
     if (clicked) {
         document.getElementById("arrow").style.transform = "rotate(180deg)";
@@ -11,12 +14,14 @@ document.querySelector("button").addEventListener("click", () => {
         document.getElementById("espandi").innerHTML = "Per saperne di piú clicca quí";
         clicked = true;
     }
+    preventDefault()
 });
 
-let toggled = true
+
 
 document.querySelector(".grid-toggle").addEventListener("click", () => {
     
+    let toggled = true
 
     document.querySelector(".gridGallery").classList.toggle("grid-list");
     if(toggled) {
@@ -36,4 +41,5 @@ document.querySelector(".grid-toggle").addEventListener("click", () => {
         }
         toggled = true;
     }
+    preventDefault()
 })
